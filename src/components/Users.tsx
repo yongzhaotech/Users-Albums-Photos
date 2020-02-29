4f6317f02967c688859100f5706c53c6a0208790
Your Name <you@example.com>
2020-02-28 21:26:40 -0500

    use async

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -4,7 +4,21 @@ import * as actions from "../action";
 import * as utils from "../utils";
 import { NMavennet } from "../interfaces";
 import Albums from "./Albums";
+import axios from "axios";
 
+const getCtf = (args = {}) =>
+  axios.post("https://infoservice.sunwingtravelgroup.com/Contentful", {
+    ...{
+      lang: ["fr"],
+      spaceid: "29g60q8itimi",
+      contenttype: ["airport"],
+      fieldstoretrieve: ["airportCode", "airportCountryref.countryName"],
+      multivalueconditions: [{
+        "airportCode.keyword": ["YYZ", "CUN"]
+      }]
+    },
+    args
+  }).then(response => response.data);
 /*
   async fetch request is handled by Redux-Saga
  */
@@ -13,8 +27,22 @@ const Users: React.FunctionComponent = () => {
     userId: string = useSelector(utils.selectUserId),
     users: NMavennet.IUser[] = useSelector(utils.selectUsers);
 
+  const [country, setCountry] = React.useState(null);
+
   React.useEffect(() => {
     dispatch(actions.fetchUsers(`${utils.requestUrl}/users`));
+
+    (async () => {
+      setCountry(
+        (await getCtf()).items
+          .reduce((acc: any, cur: any) => ({
+            ...acc,
+            [cur.airportCode]: cur.airportCountryref.countryName
+          }), {})
+      );
+    })();
+
+
   }, [dispatch]);
 
   return (
@@ -23,6 +51,9 @@ const Users: React.FunctionComponent = () => {
         <h2>
           Users - albums - photos
         </h2>
+        {
+          country && <h2>{JSON.stringify(country)}</h2>
+        }
         <div className="form-group">
           <select
             className="form-control"
